63f20dff9d25a10d4b90914b78aaf933b9f8fe51
Randy Yin <randyyin28@gmail.com>
2019-01-18 15:13:28 -0800

    Create DirectionsItem.java

---
+++ app/src/main/java/com/androidclass/bhupen/emergencycallingapp/DirectionsItem.java
@@ -0,0 +1,19 @@
+package com.androidclass.bhupen.emergencycallingapp;
+
+public class DirectionsItem {
+    String encodedpoly;
+
+    public DirectionsItem(String encodedpoly) {
+        this.encodedpoly = encodedpoly;
+    }
+
+    public String getEncodedpoly() {
+        return encodedpoly;
+    }
+
+    public void setEncodedpoly(String encodedpoly) {
+        this.encodedpoly = encodedpoly;
+    }
+
+
+}
